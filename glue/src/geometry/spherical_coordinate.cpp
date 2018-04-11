1a6fd68170a8ba7102a2fa52e725ca1aa8f32764
Mustafa Işık <mustafaisikmetu@gmail.com>
2018-04-11 05:05:43 +0300

    Cartesian to spherical construction is added. More intuitive variable names.

--- glue/src/geometry/spherical_coordinate.cpp
+++ glue/src/geometry/spherical_coordinate.cpp
@@ -19,6 +19,15 @@ namespace glue
 			, w(p_w)
 		{}
 
+		SphericalCoordinate::SphericalCoordinate(const glm::vec3& direction, const OrthonormalBasis& basis)
+		{
+			glm::vec3 uvw_coordinate(glm::dot(direction, basis.u), glm::dot(direction, basis.v), glm::dot(direction, basis.w));
+
+			radius = glm::length(uvw_coordinate);
+			theta = glm::atan(glm::sqrt(uvw_coordinate.x * uvw_coordinate.x + uvw_coordinate.y * uvw_coordinate.y) / uvw_coordinate.z);
+			phi = glm::atan(uvw_coordinate.y / uvw_coordinate.x);
+		}
+
 		SphericalCoordinate::SphericalCoordinate(float p_radius, float p_theta, float p_phi)
 			: radius(p_radius)
 			, theta(p_theta)
@@ -28,9 +37,9 @@ namespace glue
 		glm::vec3 SphericalCoordinate::convertToCartesian(const OrthonormalBasis& basis) const
 		{
 			auto sin_theta = glm::sin(theta);
-			auto magnitude = glm::vec3(glm::cos(phi) * sin_theta, glm::sin(phi) * sin_theta, glm::cos(theta)) * radius;
+			auto uvw_coordinate = glm::vec3(glm::cos(phi) * sin_theta, glm::sin(phi) * sin_theta, glm::cos(theta)) * radius;
 
-			return basis.u * magnitude.x + basis.v * magnitude.y + basis.w * magnitude.z;
+			return basis.u * uvw_coordinate.x + basis.v * uvw_coordinate.y + basis.w * uvw_coordinate.z;
 		}
 	}
 }
--- glue/src/geometry/spherical_coordinate.h
+++ glue/src/geometry/spherical_coordinate.h
@@ -13,7 +13,9 @@ namespace glue
 			glm::vec3 v;
 			glm::vec3 w;
 
+			//p_w is assumed to be normalized.
 			OrthonormalBasis(const glm::vec3& p_w);
+			//p_u, p_v and p_w are assumed to establish an orthonormal basis.
 			OrthonormalBasis(const glm::vec3& p_u, const glm::vec3& p_v, const glm::vec3& p_w);
 		};
 
@@ -23,6 +25,7 @@ namespace glue
 			float theta;
 			float phi;
 
+			SphericalCoordinate(const glm::vec3& direction, const OrthonormalBasis& basis);
 			SphericalCoordinate(float p_radius, float p_theta, float p_phi);
 
 			glm::vec3 convertToCartesian(const OrthonormalBasis& basis) const;
