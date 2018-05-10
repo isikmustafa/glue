990657c4c924293e2ead5ca081a90db0c5d45d3c
Mustafa Işık <mustafaisikmetu@gmail.com>
2018-05-11 01:50:27 +0300

    filters work in 3x3 windows

--- glue/src/core/filter.cpp
+++ glue/src/core/filter.cpp
@@ -7,7 +7,7 @@ namespace glue
 	{
 		std::unique_ptr<RealSampler> BoxFilter::generateSampler() const
 		{
-			return std::make_unique<UniformSampler>(0.0f, 1.0f);
+			return std::make_unique<UniformSampler>(-1.0f, 2.0f);
 		}
 
 		GaussianFilter::GaussianFilter(float sigma)
@@ -16,7 +16,7 @@ namespace glue
 
 		std::unique_ptr<RealSampler> GaussianFilter::generateSampler() const
 		{
-			return std::make_unique<GaussianSampler>(0.5f, 0.5f, m_sigma);
+			return std::make_unique<GaussianSampler>(0.5f, 1.5f, m_sigma);
 		}
 	}
 }
