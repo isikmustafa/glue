ef7bcc10bf148b67cb6324b37754d419cc6eaa7b
Mustafa Işık <mustafaisikmetu@gmail.com>
2018-10-28 00:18:47 +0200

    corrections and simplifications missing

--- glue/src/material/smooth_layered.cpp
+++ glue/src/material/smooth_layered.cpp
@@ -33,14 +33,16 @@ namespace glue
 			: m_kd(xml.kd->create())
 			, m_ior_n(xml.ior_n)
 			, m_fsum(0.0f)
-			, m_sinthetac(glm::sin(microfacet::fresnel::Dielectric().criticAngle(1.0f / m_ior_n)))
 		{
 			core::UniformSampler sampler;
 			constexpr int N = 100000;
 			for (int i = 0; i < N; ++i)
 			{
-				auto dir = geometry::SphericalCoordinate(1.0f, glm::acos(glm::sqrt(sampler.sample())), glm::two_pi<float>() * sampler.sample()).toCartesianCoordinate();				m_fsum += microfacet::fresnel::Dielectric()(1.0f / m_ior_n, core::math::cosTheta(dir));
-			}
+				auto dir = geometry::SphericalCoordinate(1.0f, glm::acos(glm::sqrt(sampler.sample())), glm::two_pi<float>() * sampler.sample()).toCartesianCoordinate();
+
+				m_fsum += microfacet::fresnel::Dielectric()(1.0f / m_ior_n, core::math::cosTheta(dir));
+			}
+
 			m_fsum /= N;
 		}
 
@@ -66,14 +68,10 @@ namespace glue
 					break;
 
 				case 1:
-					//Multiplying by m_sinthetac restricts theta to be in [0, theta_critical].
-					auto theta = glm::asin(glm::sqrt(sampler.sample()) * m_sinthetac);
-					auto wo_inside = geometry::SphericalCoordinate(1.0f, theta, glm::two_pi<float>() * sampler.sample()).toCartesianCoordinate();
-
-					wo = glm::refract(wo_inside, glm::vec3(0.0f, 0.0f, -1.0f), m_ior_n);
+					wo = geometry::SphericalCoordinate(1.0f, glm::acos(glm::sqrt(sampler.sample())), glm::two_pi<float>() * sampler.sample()).toCartesianCoordinate();
 					auto f_in = microfacet::fresnel::Dielectric()(m_ior_n, core::math::cosTheta(wo));
 					auto kd = m_kd->fetch(intersection);
-					f = (1.0f - f_in) * kd * (m_sinthetac * m_sinthetac) / (1.0f - m_fsum * kd);
+					f = 1.0f / (m_ior_n * m_ior_n) * (1.0f - f_in) * kd / (1.0f - m_fsum * kd);
 					break;
 				}
 			}
@@ -93,17 +91,9 @@ namespace glue
 					return glm::vec3(f_out) / core::math::cosTheta(wo_tangent);
 
 				case 1:
-					auto wo_inside = -glm::refract(-wo_tangent, glm::vec3(0.0f, 0.0f, 1.0f), 1.0f / m_ior_n);
-
-					//Does not obey reciprocity.
-					auto dwins_over_dwi = (1.0f / (m_ior_n * m_ior_n)) * glm::abs(glm::dot(wo_tangent, wo_inside));
 					auto f_in = microfacet::fresnel::Dielectric()(m_ior_n, core::math::cosTheta(wo_tangent));
-
 					auto kd = m_kd->fetch(intersection);
-					auto nom = (1.0f - f_in) * (1.0f - f_out) * core::math::cosTheta(wo_inside) * kd;
-					auto denom = glm::pi<float>() * (1.0f - m_fsum * kd) * core::math::cosTheta(wo_tangent);
-
-					return dwins_over_dwi * nom / denom;
+					return 1.0f / (m_ior_n * m_ior_n) * (1.0f - f_in) * (1.0f - f_out) * kd * glm::one_over_pi<float>() / (1.0f - m_fsum * kd);
 				}
 			}
 
@@ -114,13 +104,14 @@ namespace glue
 		{
 			if (core::math::cosTheta(wi_tangent) > 0.0f && core::math::cosTheta(wo_tangent) > 0.0f)
 			{
-				if (intersection.bsdf_choice == 1)
+				switch (intersection.bsdf_choice)
 				{
-					auto wo_inside = -glm::refract(-wo_tangent, glm::vec3(0.0f, 0.0f, 1.0f), 1.0f / m_ior_n);
-					auto fresnel = microfacet::fresnel::Dielectric()(m_ior_n, core::math::cosTheta(wi_tangent));
-					auto dwins_over_dwi = (1.0f / (m_ior_n * m_ior_n)) * glm::abs(glm::dot(wo_tangent, wo_inside));
+				case 0:
+					return 0.0f;
 
-					return (1.0f - fresnel) * dwins_over_dwi * core::math::cosTheta(wo_inside) * glm::one_over_pi<float>() / (m_sinthetac * m_sinthetac);
+				case 1:
+					auto fresnel = microfacet::fresnel::Dielectric()(m_ior_n, core::math::cosTheta(wi_tangent));
+					return (1.0f - fresnel) * core::math::cosTheta(wo_tangent) * glm::one_over_pi<float>();
 				}
 			}
 
@@ -134,7 +125,7 @@ namespace glue
 
 		bool SmoothLayered::useMultipleImportanceSampling(const geometry::Intersection& intersection) const
 		{
-			return false;
+			return intersection.bsdf_choice == 1;
 		}
 	}
 }
